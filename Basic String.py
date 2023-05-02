3a2307a447a6c422b889a102ff8f6751f9a9f085
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-02 20:18:33 +0530

    Basic String.py
    Basic String operations

---
+++ Basic String.py
@@ -0,0 +1,13 @@
+# basic string operations
+name="RAFEEQ"
+Name="sameer"
+print(name)
+print(Name)
+print(len(name))
+print(len(Name))
+print(name[0])
+print(Name[5])
+print(name[0:4])
+print(Name[1:])
+print(name[:6])
+print(Name[::-1])
