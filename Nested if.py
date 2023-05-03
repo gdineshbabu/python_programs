1b93329d127b9b15a76a445d90d8b19ce765c6b5
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-03 17:40:28 +0530

    Nested if.py
    with in one if condition there must exist an another if condition
    
    input:
    enter a number:10
    enter a number:20
    enter a number:30
    
    output: c ic greater value

---
+++ Nested if.py
@@ -0,0 +1,18 @@
+# Nested if
+
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+c=int(input("enter a number:"))
+
+if a>b:
+    if a>c:
+        print("a is greter value")
+    else:
+        print("c is greter value")
+else:
+    if b>c:
+        print("b is greater value")
+    else:
+        print("c is greater value")
+    
+    
