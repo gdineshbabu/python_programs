a5aa10c6d7ef71a5bfb8d61667795d0756b5e80d
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-03 17:35:18 +0530

    if elif else statement.py
    if if condition true ptint if statement else if elif condition true then print elif statement else print else statement
    
    input:
    enter a number:10
    enter a number:10
    
    output:
    both are equal

---
+++ if elif else statement.py
@@ -0,0 +1,10 @@
+# if elif else
+
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+if a>b:
+    print(a,"is greater than",b)
+elif b==a:
+    print("both are equal")
+else:
+    print(b,"is greater than",a)
