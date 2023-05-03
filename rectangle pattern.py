52d714a93082737423f4a2389265047ff6a6408a
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-03 21:54:42 +0530

    rectangle pattern.py
    print the rectangle pattern
    
    input:
    enter no.of rows:5
    enter no.of colums:6
    
    output:
    ******
    ******
    ******
    ******
    ******

---
+++ rectangle pattern.py
@@ -0,0 +1,8 @@
+# rectangle pattern
+# input of 2 differnt values
+a=int(input("enter no.of rows:"))
+b=int(input("enter no.of colums:"))
+for  i in range(a):
+    for j in range(b):
+        print("*",end="")
+    print("")
