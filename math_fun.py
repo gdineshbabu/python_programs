46c622624f8a96ffb87810c36944aad8b3aed06c
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-05 10:55:17 +0530

    math_fun.py
    mathematical functions
    
    output:
    11
    10
    20.0
    10.0
    3628800
    11
    100.0
    60.0
    2
    10.0
    0.8509035245341184
    1.0
    1.6197751905438615

---
+++ math_fun.py
@@ -0,0 +1,18 @@
+# mathematical functions
+import math as m
+a=10
+b=20
+print(math.ceil(10.2))
+print(math.floor(10.9))
+print(math.fabs(b))
+print(math.copysign(a,b))
+print(math.factorial(10))
+print(math.ceil(10.2))
+print(math.pow(a,2))
+l=[10,20,30]
+print(m.fsum(l))
+print(m.gcd(10,2))
+print(m.sqrt(100))
+print(m.sin(45))
+print(m.cos(0))
+print(m.tan(45))
