5e0ac1623caffae74e5a225400422d4a7ef3cb7b
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-07 21:12:50 +0530

    String_basics.py
    # Basic operations of strings
    
    hello world-welcome to strings_
    hello world-hello world-hello world-
    welcome to strings_welcome to strings_welcome to strings_
    True
    12
    19
    h
    e
    l
    l
    o
     
    w
    o
    r
    l
    d
    -
    w
    e
    l
    c
    o
    m
    e
     
    t
    o
     
    s
    t
    r
    i
    n
    g
    s
    _
    ​

---
+++ String_basics.py
@@ -0,0 +1,22 @@
+# Basic operations
+
+s='hello world-'
+s1='welcome to strings_'
+# concatenation
+print(s+s1)
+# print n times
+print(s*3)
+print(s1*3)
+# membership
+print('hello'in s)
+
+# length of string
+print(len(s))
+print(len(s1))
+
+# iteration
+
+for i in s:
+    print(i)
+for i in s1:
+    print(i)
