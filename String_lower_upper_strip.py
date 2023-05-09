126567ea4408b9cbe66608676ae254cbdd577592
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-09 20:54:33 +0530

    Create String_lower_upper_strip.py
     String Built in method - upper() and lower() and strip()
    
    input:
    
    "  Hello World!!  "
    
    output:
    
      HELLO WORLD!!  
      hello world!!  
    Hello World!!

---
+++ String_lower_upper_strip.py
@@ -0,0 +1,6 @@
+#  String Built in method - upper() and lower() and strip()
+
+s="  Hello World!!  "
+print(s.upper()) # convert all into upper case
+print(s.lower()) # convert all into lowercase
+print(s.strip()) # removes extra spaces at frent and last
