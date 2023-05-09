dc787decfaa261679d794dd48c17473e7d1182a0
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-09 20:57:39 +0530

    String_strips.py
    #  String Built in method - strip() and rstrip() and lstrip()
    
    input:
    
    s="  Hello World!!  "
    
    output:
    
    Hello World!!
      Hello World!!
    Hello World!!

---
+++ String_strips.py
@@ -0,0 +1,6 @@
+#  String Built in method - strip() and rstrip() and lstrip()
+
+s="  Hello World!!  "
+print(s.strip()) # removes extra spaces at frent and last
+print(s.rstrip()) # removes extra spaces at last
+print(s.lstrip()) # removes extra spaces at frent
