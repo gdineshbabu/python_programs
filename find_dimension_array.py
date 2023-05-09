97297b7dd61633d39b1997696cad5d85a300681b
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-09 15:08:18 +0530

    find_dimension_array.py
    ---------FIND DIMENSION OF THE ARRAY--------
    
    output::
    
    The list is :
     [1 2 3 4]
    Array dimension is : 1
    
    The list is :
     [[1 2 3 4]
     [1 2 3 4]]
    Array dimension is : 2
    
    The list is :
     [[[1 2 3 4]
      [1 2 3 4]
      [1 2 3 4]]]
    Array dimension is : 3
    
    The list is :
     [[[[1 2 3 4]
       [1 2 3 4]
       [1 2 3 4]
       [1 2 3 4]]]]
    Array dimension is : 4

---
+++ find_dimension_array.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+a=np.array([1,2,3,4])
+print("The list is :\n",a)
+print("Array dimension is :",a.ndim)
+b=np.array([[1,2,3,4],[1,2,3,4]])
+print("\nThe list is :\n",b)
+print("Array dimension is :",b.ndim)
+c=np.array([[[1,2,3,4],[1,2,3,4],[1,2,3,4]]])
+print("\nThe list is :\n",c)
+print("Array dimension is :",c.ndim)
+d=np.array([[[[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]]]])
+print("\nThe list is :\n",d)
+print("Array dimension is :",d.ndim)
