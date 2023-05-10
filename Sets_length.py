a6860f8ada0283312fbba4f1f70c38b8faecd0ca
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-10 11:55:22 +0530

    Sets_length.py
    #set in python - length
    
    # it does n't consider the duplicate elements
    
    input:
    
    s={10,10,20,20,30,30}
    
    output:
    
    3

---
+++ Sets_length.py
@@ -0,0 +1,5 @@
+#set in python - length
+s={10,10,20,20,30,30}
+print(len(s)) # it does n't consider the duplicate elements
+s={10,10,20,20,30,30}
+print(len(s)) # it does n't consider the duplicate elements
