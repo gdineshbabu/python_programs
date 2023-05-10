81ef8c0ed9fcb0f8e1d244b8635f209ac999616a
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-10 21:21:39 +0530

    Functions_local_var.py
    # Functions in python -local variables
    
    # local variable can't use as global variable without initialize if the variable is already used previously it can use that value
    
    
    output:
    
    100

---
+++ Functions_local_var.py
@@ -0,0 +1,8 @@
+# Functions in python -local variables
+
+# local variable can't use as global variable without initialize if the variable is already used previously it can use that value
+def display():
+    a=100
+    print(a)
+display()
+    
