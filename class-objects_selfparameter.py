1bad45b5d64e07f0e9ba0e5145ca8d04f2373fbc
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-05-12 20:01:14 +0530

    class-objects_selfparameter.py
    The self Parameter
    
    
    The self parameter is a reference to the current instance of the class, and is used to access variables that belongs to the class.
    
    It does not have to be named self , you can call it whatever you like, but it has to be the first parameter of any function in the class:
    
    
    output:
    
    Hello this is Dinesh Babu iam 20 years old

---
+++ class-objects_selfparameter.py
@@ -0,0 +1,8 @@
+class D:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def m(a):
+        print("Hello this is "+a.name+" iam",a.age,"years old")
+p=D("Dinesh Babu",20)
+p.m()
